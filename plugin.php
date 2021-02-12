648b48eeb129df205447b7302729e6248079f61f
Alexander Killing <killing@leifos.de>
2021-02-12 12:50:53 +0100

    updated version

--- plugin.php
+++ plugin.php
@@ -10,12 +10,12 @@
 $id = "pctpc";
 
 // code version; must be changed for all code changes
-$version = "0.0.10";
+$version = "0.0.11";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.3.0";
-$ilias_max_version = "5.5.999";
+$ilias_max_version = "7.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Fred Neumann";
